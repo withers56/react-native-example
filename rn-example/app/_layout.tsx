0d0659e801431e5576e997f4dae4b651eee4288c
William Withers <william.k.withers@gmail.com>
2025-08-20 21:04:03 -0500

    examp

--- rn-example/app/_layout.tsx
+++ rn-example/app/_layout.tsx
@@ -1,13 +1,14 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
-
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { Appearance } from "react-native";
+import { Colors } from "@/constants/Colors";
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
+  const colorScheme = Appearance.getColorScheme();
+
+  const theme = colorScheme === 'dark' ? Colors.dark : Colors.light;
+
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
@@ -18,17 +19,19 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack screenOptions={{ headerStyle: { backgroundColor: 
+        theme.headerBackground}, headerTintColor: theme.text, headerShadowVisible: false     
+      }}>
+        {/* <Stack.Screen name="(tabs)" options={{ headerShown: false }} /> */}
         {/* <Stack.Screen name="(coffee)" options={{ headerShown: false }} /> */}
-        {/* <Stack.Screen name='index' options={{ 
+        <Stack.Screen name='index' options={{ 
           title: 'Home', 
           headerShown: false }} /> 
-        <Stack.Screen name='contact' options={{ title: 'Contact us' }} /> */}
-        <Stack.Screen name="+not-found" />
+        <Stack.Screen name='menu' options={{ 
+          title: 'Coffee Shop Menu', 
+          headerShown: false }} /> 
+        <Stack.Screen name='contact' options={{ title: 'Contact us', headerShown: true }} />
+        <Stack.Screen name="+not-found" options={{ headerShown: false}}/>
       </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
   );
 }
